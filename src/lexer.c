446bad0ebe7df4e264fa8be2e445481ced7d8289
Landon Faigle <landon.faigle@gmail.com>
2026-08-14 08:59:50 -0400

    feat: define token struct and type enum

--- src/lexer.c
+++ src/lexer.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
+#define MAX_TOKENS 100
+
+enum token_type {
+	TOKEN_INT,
+	TOKEN_VOID,
+	TOKEN_ID,
+	TOKEN_LPAREN,
+	TOKEN_RPAREN,
+	TOKEN_LBRACE,
+	TOKEN_RBRACE,
+	TOKEN_SEMI,
+	TOKEN_ASSIGN,
+	TOKEN_INT_LIT,
+	TOKEN_EOF
+};
+
+struct token {
+	enum token_type type;
+	union {
+		char name[32];
+		int val;
+	};
+};
+
 int main(void)
 {
+	struct token tokens[MAX_TOKENS];
 	printf("Hello world\n");
 }
