4d2c723c97a73fe5d9934f490f904edf4075414e
Landon Faigle <landon.faigle@gmail.com>
2026-08-15 13:19:41 -0400

    refactor: reformat calloc in lexer_create_and_load

--- src/lexer.c
+++ src/lexer.c
@@ -32,7 +32,7 @@ static bool is_in_id(u8 c)
 
 struct lexer *lexer_create_and_load(const char *filename, int prog_size)
 {
-	struct lexer *l = calloc(sizeof(struct lexer), 1);
+	struct lexer *l = calloc(1, sizeof(*l));
 	l->prog = calloc(prog_size, sizeof(*l->prog));
 	FILE *f;
 	if (!(f = fopen(filename, "r"))) {
