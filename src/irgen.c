7a2f720ea4ea7db17710a9c1ea1658ff959afa4d
Landon Faigle <landon.faigle@gmail.com>
2026-08-15 10:55:20 -0400

    refactor: adopt consistent naming of struct irgen as irg

--- src/irgen.c
+++ src/irgen.c
@@ -13,50 +13,50 @@ struct irgen {
 	int pos;
 };
 
-static void irgen_stmt(struct irgen *ir, struct node *n)
+static void irgen_stmt(struct irgen *irg, struct node *n)
 {
-	if (ir->pos == MAX_INSTRS) {
+	if (irg->pos == MAX_INSTRS) {
 		fprintf(stderr, "too many IR instructions\n");
 		exit(1);
 	}
-	ir->instrs[ir->pos].i_type = INSTR_ASSIGN;
-	ir->instrs[ir->pos].d_type = n->children[0]->d_type;
-	strcpy(ir->instrs[ir->pos].dest_name, n->children[0]->name);
-	ir->instrs[ir->pos].op1_kind = OPERAND_LITERAL;
-	ir->instrs[ir->pos++].op1_val = n->children[1]->val;
+	irg->instrs[irg->pos].i_type = INSTR_ASSIGN;
+	irg->instrs[irg->pos].d_type = n->children[0]->d_type;
+	strcpy(irg->instrs[irg->pos].dest_name, n->children[0]->name);
+	irg->instrs[irg->pos].op1_kind = OPERAND_LITERAL;
+	irg->instrs[irg->pos++].op1_val = n->children[1]->val;
 }
 
-static void irgen_func(struct irgen *ir, struct node *n)
+static void irgen_func(struct irgen *irg, struct node *n)
 {
-	if (ir->pos == MAX_INSTRS) {
+	if (irg->pos == MAX_INSTRS) {
 		fprintf(stderr, "too many IR instructions\n");
 		exit(1);
 	}
-	ir->instrs[ir->pos].i_type = INSTR_FUNC_START;
-	ir->instrs[ir->pos].d_type = n->d_type;
-	strcpy(ir->instrs[ir->pos++].dest_name, n->name);
+	irg->instrs[irg->pos].i_type = INSTR_FUNC_START;
+	irg->instrs[irg->pos].d_type = n->d_type;
+	strcpy(irg->instrs[irg->pos++].dest_name, n->name);
 	for (int i = 0; i < n->children_num; i++) {
-		irgen_stmt(ir, n->children[i]);
+		irgen_stmt(irg, n->children[i]);
 	}
-	if (ir->pos == MAX_INSTRS) {
+	if (irg->pos == MAX_INSTRS) {
 		fprintf(stderr, "too many IR instructions\n");
 		exit(1);
 	}
-	ir->instrs[ir->pos++].i_type = INSTR_FUNC_END;
+	irg->instrs[irg->pos++].i_type = INSTR_FUNC_END;
 }
 
-struct instr *irgen_prog(struct irgen *ir, struct node *ast)
+struct instr *irgen_prog(struct irgen *irg, struct node *ast)
 {
 	for (int i = 0; i < ast->children_num; i++) {
-		irgen_func(ir, ast->children[i]);
+		irgen_func(irg, ast->children[i]);
 	}
-	if (ir->pos == MAX_INSTRS) {
+	if (irg->pos == MAX_INSTRS) {
 		fprintf(stderr, "too many IR instructions\n");
 		exit(1);
 	}
-	ir->instrs[ir->pos++].i_type = INSTR_EOF;
-	struct instr *instrs = malloc(ir->pos * sizeof(*instrs));
-	memcpy(instrs, ir->instrs, ir->pos * sizeof(*instrs));
+	irg->instrs[irg->pos++].i_type = INSTR_EOF;
+	struct instr *instrs = malloc(irg->pos * sizeof(*instrs));
+	memcpy(instrs, irg->instrs, irg->pos * sizeof(*instrs));
 	return instrs;
 
 }
@@ -68,13 +68,13 @@ void instr_free(struct instr *instrs)
 
 struct irgen *irgen_create_and_load(void)
 {
-	struct irgen *ir = calloc(1, sizeof(*ir));
-	ir->instrs = malloc(MAX_INSTRS * sizeof(*ir->instrs));
-	return ir;
+	struct irgen *irg = calloc(1, sizeof(*irg));
+	irg->instrs = malloc(MAX_INSTRS * sizeof(*irg->instrs));
+	return irg;
 }
 
-void irgen_free(struct irgen *ir)
+void irgen_free(struct irgen *irg)
 {
-	free(ir->instrs);
-	free(ir);
+	free(irg->instrs);
+	free(irg);
 }
