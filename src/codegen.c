839df12e661d08dbc63b40e2245b1b8796add925
Landon Faigle <landon.faigle@gmail.com>
2026-08-15 12:31:34 -0400

    feat: add regtable_store

--- src/codegen.c
+++ src/codegen.c
@@ -8,16 +8,16 @@
 #define MAX_ASM_INSTRS 100
 #define MAX_ASM_INSTR_LEN 25
 #define MAX_REGS 7
+#define REGS_BEGIN 9
 
 struct reg {
-	char reg_name[32];
 	char var_name[32];
-	int val;
+	char reg_name[4];
+	bool free;
 };
 
 struct regtable {
 	struct reg regs[MAX_REGS];
-	int pos;
 };
 
 struct codegen {
@@ -29,10 +29,30 @@ struct codegen {
 struct codegen *codegen_create(void)
 {
 	struct codegen *cg = calloc(1, sizeof(*cg));
+	for (int i = 0; i < MAX_REGS; i++) {
+		sprintf(cg->table.regs[i].reg_name, "w%d", i + REGS_BEGIN);
+		cg->table.regs[i].free = true;
+	}
 	return cg;
 }
 
 void codegen_free(struct codegen *cg)
 {
 	free(cg);
+}
+
+char *regtable_store(struct codegen *cg, char *var_name)
+{
+	for (int i = 0; i < MAX_REGS; i++) {
+		if (cg->table.regs[i].free) {
+			strcpy(cg->table.regs[i].var_name, var_name);
+			cg->table.regs[i].free = false;
+			return cg->table.regs[i].reg_name;
+		}
+		if (!strcmp(cg->table.regs[i].var_name, var_name)) {
+			return cg->table.regs[i].reg_name;
+		}
+	}
+	fprintf(stderr, "register allocation error\n");
+	exit(1);
 }
