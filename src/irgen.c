cbbf1186f32aa539452825dbf28969f5b008fa2c
Landon Faigle <landon.faigle@gmail.com>
2026-08-17 14:48:04 -0400

    feat: add else support in irgen

--- src/irgen.c
+++ src/irgen.c
@@ -68,21 +68,42 @@ static void irgen_stmt(struct irgen *irg, struct node *n);
 static void irgen_if(struct irgen *irg, struct node *n)
 {
 	struct operand cond = irgen_expr(irg, n->children[0]);
-	irg->instrs[irg->pos].op1 = cond;
 	if (irg->pos == MAX_INSTRS) {
 		fprintf(stderr, "too many IR instructions\n");
 		exit(1);
 	}
+	int false_target = irg->label_count++;
+	irg->instrs[irg->pos].op1 = cond;
 	irg->instrs[irg->pos].i_type = INSTR_JMP;
 	strcpy(irg->instrs[irg->pos].op, "f");
-	snprintf(irg->instrs[irg->pos++].dest_name, MAX_LABEL_LEN, "L%d", irg->label_count);
+	snprintf(irg->instrs[irg->pos++].dest_name, MAX_LABEL_LEN, "L%d", false_target);
 	irgen_stmt(irg, n->children[1]);
+	int end;
+	if (n->children_num == 3) {
+		if (irg->pos == MAX_INSTRS) {
+			fprintf(stderr, "too many IR instructions\n");
+			exit(1);
+		}
+		end = irg->label_count++;
+		irg->instrs[irg->pos].i_type = INSTR_JMP;
+		strcpy(irg->instrs[irg->pos].op, "j");
+		snprintf(irg->instrs[irg->pos++].dest_name, MAX_LABEL_LEN, "L%d", end);
+	}
 	if (irg->pos == MAX_INSTRS) {
 		fprintf(stderr, "too many IR instructions\n");
 		exit(1);
 	}
 	irg->instrs[irg->pos].i_type = INSTR_LABEL;
-	snprintf(irg->instrs[irg->pos++].dest_name, MAX_LABEL_LEN, "L%d", irg->label_count++);
+	snprintf(irg->instrs[irg->pos++].dest_name, MAX_LABEL_LEN, "L%d", false_target);
+	if (n->children_num == 3) {
+		irgen_stmt(irg, n->children[2]);
+		if (irg->pos == MAX_INSTRS) {
+			fprintf(stderr, "too many IR instructions\n");
+			exit(1);
+		}
+		irg->instrs[irg->pos].i_type = INSTR_LABEL;
+		snprintf(irg->instrs[irg->pos++].dest_name, MAX_LABEL_LEN, "L%d", end);
+	}
 }
 
 static void irgen_block(struct irgen *irg, struct node *n);
