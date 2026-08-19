382feb45af2674146c5bb745a7e9cf33adb95e0d
Landon Faigle <landon.faigle@gmail.com>
2026-08-19 17:43:02 -0400

    test: edit run_tests.sh to have proper cleanup

--- run_tests.sh
+++ run_tests.sh
@@ -4,25 +4,23 @@ CC="${CC:-aarch64-unknown-linux-gnu-gcc}"
 RUNNER="${RUNNER:-hl}"
 
 set -e
+current=""
+trap '
+    code=$?
+    rm -f tests/test tests/test.s tests/test.o tests/_start.o
+    if [ $code -ne 0 ]; then
+        echo "FAILED on: $current (exit $code)"
+    fi
+' EXIT
 
 make
 $CC -c tests/_start.s -o tests/_start.o
 for input in tests/inputs/*.c; do
     input=$(basename $input .c)
+    current="$input.c"
     ./compiler tests/inputs/$input.c > tests/test.s
     $CC -c tests/test.s -o tests/test.o
     $CC -nostdlib -static -no-pie tests/_start.o tests/test.o -o tests/test
-    if ! $RUNNER tests/test; then 
-        echo ERROR $input.c
-        rm tests/test
-        rm tests/test.s
-        rm tests/test.o
-        rm tests/_start.o
-        exit 1
-    fi;
+    $RUNNER tests/test
 done
-rm tests/test
-rm tests/test.s
-rm tests/test.o
-rm tests/_start.o
 echo SUCCESS
